35732f6b1973e79d64c7a3a86b6c4d808284f6ef
Karolis Kolbus <karolis.kolbus@gmail.com>
2015-03-26 23:17:40 +0000

    Initial Commit

---
+++ 2SlotMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2SlotMachine
+//
+//  Created by Karolis Kolbus on 26/03/2015.
+//  Copyright (c) 2015 Karolis Kolbus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
